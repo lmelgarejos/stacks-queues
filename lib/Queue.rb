7f784176bb761cc92cfefc0adf98f19a81994252
laura <lmelgarejos@unal.edu.co>
2017-02-23 12:28:26 -0800

    Tests for Queues and Stacks are passing

--- lib/Queue.rb
+++ lib/Queue.rb
@@ -1,22 +1,30 @@
 class Queue
   def initialize
+    @store = []
   end
 
   def enqueue(element)
+    @store.push(element)
   end
 
   def dequeue
+    @store.shift
   end
 
   def front
+    @store.first
   end
 
   def size
+    @store.length
   end
 
   def empty?
+    return true if @store.length == 0
+    return false
   end
 
   def to_s
+    return @store.to_s
   end
 end
--- lib/Stack.rb
+++ lib/Stack.rb
@@ -4,18 +4,27 @@ class Stack
   end
 
   def push(element)
+    @store.push(element)
   end
 
   def pop
+    @store.pop
+    # @store.delete(@store.last)
+    # @store.last
   end
 
   def top
+    @store.last
   end
 
   def size
+    @store.length
   end
 
   def empty?
+    #  return true if @store.length == 0
+    return size == 0
+
   end
 
   def to_s
